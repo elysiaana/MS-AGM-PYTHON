8da824afa460f99492d2d6e1428b9e9cf2b2c49f
vivi <agmeisco1998@gmail.com>
2023-07-30 16:29:45 +0700

    Calculator update v1
    small changes and improve

--- Py/Test calculator/Main.py
+++ Py/Test calculator/Main.py
@@ -1,7 +1,8 @@
 #Test calculator
 
 user = float(input('Masukan angka (input) x 5 :'))
+user = float(input('Masukan angka 5 x : '))
 
 hasil = user * 5
 
-print('Hasil dari perkalian',input,'* 5 :',hasil)
+print('Hasil dari perkalian',input,'* 5 :',hasil)print('Hasil dari perkalian','5 x',user,'=',hasil)
