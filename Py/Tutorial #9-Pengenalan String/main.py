a14ed8d82adc76b8141efaed0e6d9eea59a6f3a5
ely <agmeisco1998@gmail.com>
2023-08-24 19:37:34 +0700

    String v1

---
+++ Py/Tutorial #9-Pengenalan String/main.py
@@ -0,0 +1,50 @@
+#Pengenalan string
+
+data = "Elysia is my wife"
+
+print(data)
+print(type(data))
+
+#1. Cara membuat string 
+
+'''
+ Bijou is my wife <3
+'''
+
+print('===VVV===')
+
+#Single quote '...'
+data = "'Single quote'"
+
+print(data)
+
+#Double quote "..."
+data = '"Double quote"'
+
+print(data)
+
+#Example
+print("===Example===")
+
+print("'Single quote'")
+
+print('"Double quote"')
+
+#2. Menggunakan tanda \
+
+print('===VVV===')
+
+#Tanda ' menjadi string
+print('===Tanda (\') menjadi string===')
+
+print('Vill\'v is my wife')
+
+#Backslash use \\ for using like "C\test\"
+print('===Backslash===')
+
+print('C\\Ely\\Heart\\Love\\Nothing...')
+
+#Tab use \t for using like "Test   Hehe"
+print('===Tab===')
+
+print('Elysia is\t my wife <3')
